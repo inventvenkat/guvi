2e965b456003c0d28c95b78ebbbfb3173d11edc4
Venkatesh <inventvenkat@gmail.com>
2016-06-06 14:26:12 +0530

    Create codekata_beg_positive.c

---
+++ codekata_beg_positive.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+     
+    int main(void) {
+    	// your code goes here
+    	long long int t;
+        	scanf("%lld",&t);
+        	if(t==0)
+        	printf("zero");
+        	else if(t>0)
+        	printf("positive");
+        	else
+        	printf("negative");
+        	return 0;
+     
+    }
