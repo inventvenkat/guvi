c28235e96aaa402bd69da67ce129fe84034937ac
Venkatesh <inventvenkat@gmail.com>
2016-06-06 14:18:46 +0530

    Create Codekata_beginner_positive_or_negative.cpp

---
+++ Codekata_beginner_positive_or_negative.cpp
@@ -0,0 +1,14 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	long long int t;
+    	cin>>t;
+    	if(t==0)
+    	cout<<"zero";
+    	else if(t>0)
+    	cout<<"positive";
+    	else
+    	cout<<"negative";
+    	return 0;
+    }
